06313ba92dad4dedd06b063a8bde2cf3679727c7
Denis-Ozolin <ozolindenx@gmail.com>
2022-05-03 00:29:47 +0300

    added content, change style

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
 
-function Content({content}) {
+function Content({ content }) {
+  const { name, description, notes, scripts } = content;
+
   return (
     <div className='content'>
-      {content && <h2 className='content__title'>{content.name}</h2>}
-      <p className='content__description'></p>
-      <p className='content__text'>{content.content}</p>
-      <p className='content__code'></p>
-      <p className='content__npm'></p>
-
+      {content && <h2 className='content__title'>{name}</h2>}
+      {description && <p className='content__description'>{description}</p>}
+      {notes && <ul className='content__note-list'>
+        {notes.map(({ id, instruction, npm, link, shortcut }) => (
+          <li key={id} className='content__note'>
+            {instruction &&
+              <div className='content__box'>
+                <span className='content__marker'></span>
+                <p className='content__instruction'>{instruction}</p>
+              </div>}
+            {npm && <p className='content__npm'>{npm}</p>}
+            {link && <a href={link} className='content__link' target='_blank' rel='noreferrer'>{link}</a>}
+            {shortcut && <p className='content__shortcut'>{shortcut}</p>}
+          </li>
+        ))}
+      </ul>}
+      {scripts && <ul className='content__script-list'>
+        {scripts.map(({ id, path, code }) => (
+          <li key={id} className='content__script'>
+            <p className='content__path'>{path}</p>
+            <p className='content__code'>{code}</p>
+          </li>
+        ))}  
+      </ul>}
     </div>
   )
 }
